8d5e734ebcc56c7296baed2cd1882fce7f8d7957
AliSh <alishahmoradidev@gmail.com>
2025-09-02 21:18:12 +0330

    feat(users): add a validate method to check if the input username and password are authenticated in database

--- users/validations.py
+++ users/validations.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.models import BaseUserManager
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
 from django.contrib.auth.password_validation import validate_password
@@ -11,6 +12,8 @@ def user_create_validate(data:dict):
     email = data.get("email")
     password = data.get("password")
     confirm_password = data.get("confirm_password")
+
+    email = BaseUserManager.normalize_email(email=email)
 
     if not email or not password or not confirm_password:
         return ValidationError("All the fields are required")
@@ -33,3 +36,24 @@ def user_create_validate(data:dict):
 
     if User.objects.filter(email=email).exists():
         return ValidationError("This email address is registered")
+
+
+def user_authenticate_status(data:dict):
+
+    email = data.get("email")
+    password = data.get("password")
+
+    email = BaseUserManager.normalize_email(email=email)
+
+    if not email or not password:
+        raise ValidationError("Email and password are required")
+    
+    user = authenticate(username=email, password=password)
+
+    if user is None:
+        raise ValidationError("Invalid email or password")
+    
+    if not user.is_active:
+        raise ValidationError("User is NOT active")
+    
+    return user

e409e23ef1997863eb76db4c0a4ebbecbe09852d
AliSh <alishahmoradidev@gmail.com>
2025-09-02 21:12:29 +0330

    feat(users): add UserLoginView for returning access and refresh token if input data validated

--- users/views.py
+++ users/views.py
@@ -18,3 +18,30 @@ class UserCreateView(APIView):
         user = user_create(data=serializer.validated_data)
         return Response(UserCreateOutputSerializer(user, context={'request':request}).data, status=status.HTTP_201_CREATED)
     
+class UserLoginView(APIView):
+
+    def post(self, request):
+
+        serializer = UserLoginInputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = user_authenticate_status(data=serializer.validated_data)
+
+        if not user:
+            raise ValidationError("Invalid Username OR Password")
+
+        tokens = token_create(user=user)
+        
+        output = {
+            'request': request,
+            'username': user.username,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'access': tokens['access'],
+            'refresh': tokens['refresh'],           
+        }
+
+        output_serialzier = UserLoginOutputSerializer(output, context={'request': request})
+        return Response(output_serialzier.data, status=status.HTTP_200_OK)
+
+

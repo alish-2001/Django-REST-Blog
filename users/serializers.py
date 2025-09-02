31b1c4cc5b5e14ae25785b195df4d8a0ddeb0fe1
AliSh <alishahmoradidev@gmail.com>
2025-09-02 21:10:51 +0330

    feat(users): add login  input and outupt serializers

--- users/serializers.py
+++ users/serializers.py
@@ -6,10 +6,20 @@ class UserCreateInputSerializer(serializers.Serializer):
     password = serializers.CharField(required=True, write_only=True, max_length=50, validators=[validate_password])
     confirm_password = serializers.CharField(required=True, write_only=True, max_length=50, validators=[validate_password])
 
- 
 class UserCreateOutputSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     email = serializers.EmailField(read_only=True)
     role = serializers.CharField(read_only=True,)
     is_verified = serializers.BooleanField(read_only=True,)
     is_staff = serializers.BooleanField(read_only=True,)
+
+class UserLoginInputSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True, write_only=True, max_length=60)
+    password = serializers.CharField(required=True, write_only=True, max_length=50,)
+
+class UserLoginOutputSerializer(serializers.Serializer):
+    access = serializers.CharField(max_length=600, read_only=True)
+    refresh = serializers.CharField(max_length=2000, read_only=True)
+    username = serializers.CharField(max_length=60, read_only=True)
+    first_name = serializers.CharField(max_length=150, read_only=True)
+    last_name = serializers.CharField(max_length=150, read_only=True)

1864aeac75bf108aeb1526b9db5aedd83171f1a6
AliSh <alishahmoradidev@gmail.com>
2025-09-02 19:44:39 +0330

    feat(users): add login APIView

--- users/views.py
+++ users/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
-from rest_framework.generics import GenericAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import HttpResponse
+from rest_framework.exceptions import ValidationError
 
-from .serializers import UserCreateSerializer,UserOutputSerializer
-from .services import user_create
+from users.validations import user_authenticate_status
+from .serializers import UserCreateInputSerializer, UserLoginInputSerializer,UserCreateOutputSerializer, UserLoginOutputSerializer
+from .services import user_create, token_create
 
 # Create your views here.
 
@@ -14,9 +13,8 @@ class UserCreateView(APIView):
 
     def post(self, request):
        
-        serializer = UserCreateSerializer(data=request.data)
-        serializer.is_valid()
+        serializer = UserCreateInputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
         user = user_create(data=serializer.validated_data)
-        return Response(UserOutputSerializer(user).data, status=status.HTTP_201_CREATED)
-    
+        return Response(UserCreateOutputSerializer(user, context={'request':request}).data, status=status.HTTP_201_CREATED)
     

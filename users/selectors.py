db4f09a5461526e98e97a1b1d7cb92ccce39c899
AliSh <alishahmoradidev@gmail.com>
2025-09-02 21:59:09 +0330

    feat(users): add selector to fetch user profile info from db

---
+++ users/selectors.py
@@ -0,0 +1,8 @@
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+def get_user_object(pk:int):
+    return get_object_or_404(User, pk=pk)

4cbc358984c09fe7a59da4ee4281630c7bb34dca
AliSh <alishahmoradidev@gmail.com>
2025-09-03 15:19:32 +0330

    feat(users): add token verify endpoint using simplejwt TokenVerifyView

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView,TokenVerifyView
 
 from .views import UserCreateView, UserLoginView, UserProfileView, UsersListView, UserLogoutView
 
@@ -9,7 +9,8 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('users/', UsersListView.as_view(), name='users-list'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('logout/', UserLogoutView.as_view(), name='user-logout'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token-verify'),
 
 ]

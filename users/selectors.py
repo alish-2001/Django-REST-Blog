340d75bdae299734de0461133b851d69df42bebb
AliSh <alishahmoradidev@gmail.com>
2025-09-14 16:28:30 +0330

    fix(users): change selectors' exceptions and data that will be returned

--- users/selectors.py
+++ users/selectors.py
@@ -1,17 +1,17 @@
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
+from rest_framework.exceptions import NotFound as DRFNotFound
 
 from .models import OTPVerification
 
 User = get_user_model()
 
-def get_user_object(pk:int):
+def get_user_object_by_pk(pk:int):
 
     try:
         return User.objects.get(pk=pk)
     except User.DoesNotExist:
-        raise ValidationError("User Not Found")
-
+        raise DRFNotFound("User Not Found")
+    
 def get_users_queryset():
     return User.objects.all().order_by('-is_active')
 
@@ -19,7 +19,9 @@ def get_latest_user_otp_obj(user):
 
     otp = OTPVerification.objects.filter(user=user).order_by('-created_at').first()
     if otp is None:
-        raise ValidationError("No Code Found For This User, Request A New Code.")
+        raise DRFNotFound("No Code Found For This User, Request A New Code.")
     
+    return otp
+
 def check_user_existence(email):
     return User.objects.filter(email=email).exists()

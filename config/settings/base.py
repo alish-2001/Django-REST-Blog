fdf8400a90bd42b6c106ccd2b61219c6c5cb7c54
AliSh <alishahmoradidev@gmail.com>
2025-09-14 16:18:51 +0330

    fix(config): change jwt header type to Bearer

--- config/settings/base.py
+++ config/settings/base.py
@@ -140,7 +140,7 @@ SIMPLE_JWT={
 
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
-    'AUTH_HEADER_TYPES' : ("JWT",),
+    'AUTH_HEADER_TYPES' : ("Bearer",),
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=2)
 }

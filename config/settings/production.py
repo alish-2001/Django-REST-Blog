88f938419cdf0d49823fc3b2120f34652adc97c4
AliSh <alishahmoradidev@gmail.com>
2025-09-26 13:35:13 +0330

    feat(config): insall and configure django CORS headers

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,6 @@
 from .base import *
 import dj_database_url
+from corsheaders.defaults import default_headers
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env.production'))
 
@@ -11,10 +12,29 @@ CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 
+INSTALLED_APPS += [
+    "corsheaders",
+]
+
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 DATABASES = {   
     'default': dj_database_url.config(default=env("DJANGO_PG_DATABASE_URL"))
 }
 
+CORS_ALLOWED_ORIGINS = env.list("DJANGO_CORS_ALLOWED_ORIGINS")
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_HEADERS = list(default_headers) + ["authorization"]
 
 EMAIL_BACKEND = env("DJANGO_EMAIL_BACKEND")
 EMAIL_HOST = env("DJANGO_EMAIL_HOST")
